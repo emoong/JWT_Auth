2e37e5be682a34c1a366a87ce285c3363e8e2b2d
eziong <bsjwoong@gmail.com>
2021-09-22 14:34:20 +0900

    structure

---
+++ envirionment.d.ts
@@ -0,0 +1,12 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      REFRESH_TOKEN_SECRET: string;
+      ACCESS_TOKEN_SECRET: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export { }
